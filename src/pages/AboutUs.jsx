db72838219e7eaf618c5868135c55f3ec86697d9
BrandonSGC <brangmz20@gmail.com>
2024-06-08 14:07:33 -0600

    About Us Section

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import { AboutUsSection } from "../layouts";
 
 export const AboutUs = () => {
   return (
-    <div>AboutUs</div>
-  )
-}
+    <>
+      <AboutUsSection />
+    </>
+  );
+};
