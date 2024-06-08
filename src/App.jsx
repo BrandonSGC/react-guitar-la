596ebc88c478a727e2b6b369cf7289d19de5489a
BrandonSGC <brangmz20@gmail.com>
2024-06-07 20:33:53 -0600

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import { Route, Routes } from "react-router-dom"
-import { Home, AboutUs, Blog, Store } from "./pages/"
+import { Route, Routes } from "react-router-dom";
+import { Home, AboutUs, Blog, Store } from "./pages/";
+import { Header } from "./layouts";
 
 function App() {
-
   return (
-    <Routes>
-      <Route path="/home" element={<Home />}/>
-      <Route path="/about" element={<AboutUs />}/>
-      <Route path="/blog" element={<Blog />}/>
-      <Route path="/store" element={<Store />}/>
-      <Route path="/*" element={<Home />}/>
-    </Routes>
-  )
+    <>
+      <Header />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/layouts/Header.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import { Container, NavBar } from "../components";
+import { Link, useLocation } from "react-router-dom";
+
+export const Header = () => {
+  const [isHome, setIsHome] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    pathname === "/" ? setIsHome(true) : setIsHome(false);
+  }, [pathname]);
+
+  return (
+    <header className="relative py-12 bg-center bg-cover bg-header-gradient">
+      <Container>
+        <div className="flex items-center justify-between gap-10">
+          <Link to={"/"}>
+            <img
+              className="w-[200px] md:w-full"
+              src="/img/logo.svg"
+              alt="logo"
+            />
+          </Link>
+
+          <NavBar />
+        </div>
+
+        {isHome && (
+          <div className="w-3/4 my-20 space-y-6 text-white lg:w-1/2">
+            <h1 className="font-black text-7xl">Modelo VAI</h1>
+            <p className="text-xl">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+              nostrum exercitationem tenetur ducimus. Rerum ipsum beatae odio,
+              sapiente harum hic.
+            </p>
+            <p className="text-6xl font-black text-yellow-600">$399</p>
+            <Link className="inline-block px-8 py-2 font-bold uppercase border border-yellow-600 hover:bg-yellow-600">
+              Ver Producto
+            </Link>
+          </div>
+        )}
+      </Container>
+
+      {isHome && (
+        <img
+          className="absolute bottom-0 right-0 hidden lg:block"
+          src="/img/header_guitarra.png"
+          alt="guitarra header"
+        />
+      )}
+    </header>
+  );
+};
